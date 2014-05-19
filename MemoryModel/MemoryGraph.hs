3f845e63b39e788e4f43a75cf450a0f05fd5d0a1
Henning Günther <guenther@forsyte.at>
2014-05-19 16:08:09 +0200

    Remove strict maps

--- MemoryModel/MemoryGraph.hs
+++ MemoryModel/MemoryGraph.hs
@@ -5,8 +5,8 @@ import MemoryModel
 import TypeDesc
 import Value
 
-import Data.Map.Strict (Map)
-import qualified Data.Map.Strict as Map
+import Data.Map (Map)
+import qualified Data.Map as Map
 import Data.Set (Set)
 import qualified Data.Set as Set
 
