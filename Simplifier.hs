68eefc2099c9bb9ed36f639d7439072ff115fb42
Henning Günther <guenther@forsyte.at>
2014-09-12 18:31:05 +0200

    Adapt simplifier to smtlib2 changes

--- Simplifier.hs
+++ Simplifier.hs
@@ -1,10 +1,10 @@
 module Simplifier where
 
-import Data.Typeable
 import Data.List (nub)
 
 import Language.SMTLib2
 import Language.SMTLib2.Internals
+import Language.SMTLib2.Internals.Operators
 
 isFalse :: SMTExpr Bool -> Bool
 isFalse (Const False _) = True
@@ -15,30 +15,11 @@ isTrue (Const True _) = True
 isTrue _ = False
 
 simplifier :: SMTExpr Bool -> SMTExpr Bool
-simplifier (App f xs)
-  = case cast f of
-  Just And -> case cast xs of
-    Just ys -> case simplifyAnd ys of
-      Nothing -> Const False ()
-      Just [] -> Const True ()
-      Just [x] -> x
-      Just xs -> App (SMTLogic And) xs
-  Just Or -> case cast xs of
-    Just ys -> case simplifyOr ys of
-      Nothing -> Const True ()
-      Just [] -> Const False ()
-      Just [x] -> x
-      Just xs -> App (SMTLogic Or) (nub xs)
-  Just _ -> App f xs
-  Nothing -> case f of
-    SMTNot -> case cast xs of
-      Just x -> let x' = simplifier x
-                in if isFalse x'
-                   then Const True ()
-                   else (if isTrue x'
-                         then Const False ()
-                         else App SMTNot x')
-    _ -> App f xs
+simplifier (App (SMTLogic And) xs) = case simplifyAnd xs of
+  Nothing -> Const False ()
+  Just [] -> Const True ()
+  Just [x] -> x
+  Just xs -> App (SMTLogic And) xs
   where
     simplifyAnd [] = Just []
     simplifyAnd (x:xs) = let x' = simplifier x
@@ -49,6 +30,13 @@ simplifier (App f xs)
                                   else (case simplifyAnd xs of
                                            Nothing -> Nothing
                                            Just xs' -> Just (x':xs')))
+
+simplifier (App (SMTLogic Or) xs) = case simplifyOr xs of
+  Nothing -> Const True ()
+  Just [] -> Const False ()
+  Just [x] -> x
+  Just xs' -> App (SMTLogic Or) (nub xs')
+  where
     simplifyOr [] = Just []
     simplifyOr (x:xs) = let x' = simplifier x
                         in if isTrue x'
@@ -58,4 +46,13 @@ simplifier (App f xs)
                                  else (case simplifyOr xs of
                                           Nothing -> Nothing
                                           Just xs' -> Just (x':xs')))
+
+simplifier (App SMTNot x)
+  = let x' = simplifier x
+    in if isFalse x'
+       then Const True ()
+       else (if isTrue x'
+             then Const False ()
+             else App SMTNot x')
+simplifier (App f xs) = App f xs
 simplifier x = x
